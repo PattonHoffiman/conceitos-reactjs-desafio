35558d21cb0e55f0125b69a7243fc016c1867482
Patton Hoffiman <PattonHoffiman@gmail.com>
2020-10-28 17:34:28 -0300

    Can Create a New Repository

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setRepositories(res.data);
+      console.log(res.data);
+    })
+  }, []);
+  
   async function handleAddRepository() {
-    // TODO
+    const res = await api.post('repositories', {
+      title: 'Repository Title',
+      url: 'http://repository.com',
+      techs: ['Tech 1', 'Tech 2', 'Tech 3'],
+    });
+
+    const repository = res.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +31,13 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
